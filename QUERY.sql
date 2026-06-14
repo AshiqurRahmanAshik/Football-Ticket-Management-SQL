7bf4ce30e28a2c0ebca7785ef6f895684a960442
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-06-14 09:51:58 +0600

    Query 3

--- QUERY.sql
+++ QUERY.sql
@@ -102,3 +102,11 @@ SELECT user_id, full_name, email
 FROM Users
 WHERE full_name ILIKE 'Tanvir%'
 OR full_name ILIKE '%Haque%';
+
+-- =========================================================================
+-- QUERY 3
+-- =========================================================================
+SELECT booking_id, user_id, match_id,
+COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM Bookings
+WHERE payment_status IS NULL;
