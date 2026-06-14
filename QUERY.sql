6d5905bd643e0542971774cef35f207ccc1be3ad
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-06-14 09:47:29 +0600

    Create User Table

--- QUERY.sql
+++ QUERY.sql
@@ -11,18 +11,16 @@ DROP TABLE IF EXISTS Matches;
 DROP TABLE IF EXISTS Users;
 
 -- =========================================================================
--- 1. CREATE USERS TABLE
+-- 1. USERS TABLE
 -- =========================================================================
 CREATE TABLE Users (
-    user_id TYPE,
-    full_name TYPE,
-    email TYPE,
-    role TYPE,
-    phone_number TYPE,
+    user_id INT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(20),
     
-    -- Write your constraint to make 'user_id' the Primary Key
-    -- Write your constraint to ensure 'email' values are never duplicated
-    -- Write your check constraint to restrict 'role' to specific allowed strings
+    CONSTRAINT check_role CHECK (role IN ('Football Fan', 'Ticket Manager'))
 );
 
 -- =========================================================================
