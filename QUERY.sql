f8998331c71f06cf846885e2c94e6b813ba003fa
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-06-14 09:53:03 +0600

    Query 6

--- QUERY.sql
+++ QUERY.sql
@@ -132,3 +132,12 @@ SELECT
     b.booking_id AS booking_id
 FROM Users u
 LEFT JOIN Bookings b ON u.user_id = b.user_id;
+
+-- =========================================================================
+-- QUERY 6
+-- =========================================================================
+SELECT booking_id, match_id, total_cost
+FROM Bookings
+WHERE total_cost > (
+    SELECT AVG(total_cost) FROM Bookings
+);
